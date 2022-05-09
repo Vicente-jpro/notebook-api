98d238da17c87e2900ace2fe67400c6d73763ffb
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-05-09 23:48:20 +0100

    Kind contact added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :kinds
   resources :contacts
    root "contacts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+puts "Cadastrado dos tipos de contactos..."
+3.times do |i| 
+	Kind.create!(
+		description: "Amizade"
+	)
+	Kind.create!(
+		description: "Trabalho"
+	)
+	Kind.create!(
+		description: "Jogo"
+	)
+end
+
+puts "Tipos de contactos cadastrado com sucesso."
+
+
+
+
 puts "Cadastrado os contactos..."
 100.times do |i| 
 	Contact.create!(
 		name: Faker::Name.name,
 		email: Faker::Internet.email,
-		birthdate: Faker::Date.between(from: '1990-09-23', to: '2022-09-25')
+		birthdate: Faker::Date.between(from: '1990-09-23', to: '2022-09-25'),
+		kind_id: 1
 	)
 end
 
 puts "Contactos cadastrado com sucesso."
+
