31684dc501c1d4ab2906b24a52076d786a9f0f83
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-05-12 18:05:14 +0100

    Contact tranlated into portuguese

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
 
   # GET /contacts/1
   def show
-    render json: @contact
+    render json: { contact:  @contact.in_portuguese, message: "Success." }
   end
 
   # POST /contacts
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,11 +3,19 @@ class Contact < ApplicationRecord
 	def author
 	  "Vicente Rúben Xavier Simão"
 	end
-	def as_json(options={})
-	  super(
-	  	root: true,
-	  	methods: :author, 
-	  	include: {kind: { only: [:description]  }}
-	  	)
+	# def as_json(options={})
+	#   super(
+	#   	root: true,
+	#   	methods: :author, 
+	#   	include: {kind: { only: [:description]  }}
+	#   	)
+	# end
+
+	def in_portuguese
+	 {
+	 	nome: self.name,
+	 	email: self.email,
+	 	data_nascimento: self.birthdate
+	 } 
 	end
 end
