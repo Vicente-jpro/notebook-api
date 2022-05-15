c1c6212b6fc9967861a6802ddfbdffbfa2245192
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-05-15 18:48:51 +0100

    Content type to controller application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+	before_action :ensure_json_request
+
+	def ensure_json_request
+	  return if request.headers["Accept"] =~ /json/
+	  render json: { 
+	  	notification:"You missing to specify the headers like:", 
+	  	your_spesification_should_be_like: {
+	  		headers_or_key: "Accept",
+	  		content_type_or_value: "application/json"
+	  	}
+	  }, status: 406
+	end
 end
